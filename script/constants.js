10ac87321d884bf7edc31c64c30ee8b0562326a2
mmKALLL <esakoskin2@gmail.com>
2017-08-23 20:06:32 +0300

    Ordered source files a bit. Reading on how to integrate with PixiJS.

--- script/constants.js
+++ script/constants.js
@@ -1,14 +1,10 @@
 "use strict";
 
 /* *****************************************************************************
- * Invasion - A game about defending your planet and escaping solitude.
+ * One-Armed Assault - Game about travelling a hostile world.
  * Author: Esa Koskinen (mmKALLL)
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * Copyright (c) 2017 Esa Koskinen
+ * Copyright (c) 2017 Esa Koskinen, all rights reserved
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
