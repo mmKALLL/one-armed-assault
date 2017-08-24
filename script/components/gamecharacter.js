0039020edd2d34f3c10e09a3fd6f1b18cd080c95
mmKALLL <esakoskin2@gmail.com>
2017-08-24 18:23:25 +0300

    Added lots of structure regarding ECS.

---
+++ script/components/gamecharacter.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/* *****************************************************************************
+ * One-Armed Assault - Game about travelling a hostile world.
+ * Author: Esa Koskinen (mmKALLL)
+ *
+ * Copyright (c) 2017 Esa Koskinen, all rights reserved
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * *****************************************************************************/
+ 
+ // HP, sprite, etc
+ ECS.Components.WeaponSkills = (function WeaponSkills() {
+   
+   return {};
+ })();
---
+++ script/entities/Entity.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/* *****************************************************************************
+ * One-Armed Assault - Game about travelling a hostile world.
+ * Author: Esa Koskinen (mmKALLL)
+ *
+ * Copyright (c) 2017 Esa Koskinen, all rights reserved
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * *****************************************************************************/
+ 
+ 
+ECS.Entity = function Entity() {
+  // TODO: Generate a pseudo random ID
+  this.id = (+new Date()).toString(16) +
+  (Math.random() * 100000000 | 0).toString(16) +
+  ECS.Entity.prototype._count;
+  
+  // increment counter
+  ECS.Entity.prototype._count++;
+  
+  // The component data will live in this object
+  this.components = {};
+  
+  return this;
+};
