c174e895523ec62582855620c5e27ed15891765e
mmKALLL <esakoskin2@gmail.com>
2017-08-25 03:58:20 +0300

    Started working on ECS a bit more.

--- script/entities/Entity.js
+++ script/entities/Entity.js
@@ -17,7 +17,7 @@
  
  
 ECS.Entity = function Entity() {
-  // TODO: Generate a pseudo random ID
+  // TODO: Generate a proper ID
   this.id = (+new Date()).toString(16) +
   (Math.random() * 100000000 | 0).toString(16) +
   ECS.Entity.prototype._count;
